1dbe2c256a54fc3540c7a0e763d705bd1848d59b
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-05-04 15:36:47 +0200

    useSponsors without Promise

--- client/src/views/Sponsors/useSponsors.ts
+++ client/src/views/Sponsors/useSponsors.ts
@@ -5,23 +5,21 @@ export function useSponsors() {
     const [sponsors, setSponsors]: [string[][], Function] = useState([]);
     const [error, setError]: [string | null, Function] = useState(null);
 
-    const importSponsors = (event: ChangeEvent<HTMLInputElement>): Promise<void> =>
-        new Promise((resolve, reject) => {
-            Papa.parse(event.target.files?.[0], {
-                complete: function (results) {
-                    if (results.errors.length > 0) {
-                        setError('Error trying to parse the file');
-                        reject();
-                    } else {
-                        setSponsors(results.data);
-                        resolve();
-                    }
-                },
-                error: function (error) {
-                    setError(error.message);
-                    reject();
-                },
-            });
+    const importSponsors = (event: ChangeEvent<HTMLInputElement>): void =>
+        Papa.parse(event.target.files?.[0], {
+            complete: function (results) {
+                if (results.errors.length > 0) {
+                    setError('Error trying to parse the file');
+                    setSponsors([]);
+                } else {
+                    setError(null);
+                    setSponsors(results.data);
+                }
+            },
+            error: function (error) {
+                setError(error.message);
+                setSponsors([]);
+            },
         });
     return {
         sponsors,
