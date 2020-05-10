e0e87ea8f1f110f80be207869a248fcdaf27c2b6
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-05-10 14:29:55 +0200

    New sponsor init

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -5,6 +5,9 @@ export default {
     importSponsors(sponsors: Sponsor[]) {
         return axios.post('/persons-import', sponsors);
     },
+    saveSponsors(sponsor: Sponsor) {
+        return axios.post('/persons', sponsor);
+    },
     getSponsors(search) {
         const query = new URLSearchParams();
         query.append('search', search);
--- client/src/views/Sponsors/SponsorsEdit.tsx
+++ client/src/views/Sponsors/SponsorsEdit.tsx
@@ -3,9 +3,20 @@ import Title from '../../components/Title';
 import Button from '../../components/Button';
 import { useHistory } from 'react-router-dom';
 import Input from '../../components/Input';
+import useForm from '../../hooks/useForm';
+import api from '../../services/api';
 
 function SponsorsEdit() {
     const history = useHistory();
+    const { values, bindInput } = useForm({ name: '', email: '' });
+    const save = async () => {
+        try {
+            await api.saveSponsors(values);
+            history.push('/sponsors');
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div>
@@ -13,13 +24,13 @@ function SponsorsEdit() {
             <div className="flex justify-between py-4">
                 <div></div>
                 <div>
-                    <Button label="Save" onClick={() => console.log('...')} />
+                    <Button label="Save" onClick={save} />
                     <Button label="Cancel" onClick={() => history.push('/sponsors')} />
                 </div>
             </div>
             <div className="w-1/2">
-                <Input label="Name" className="mb-2" />
-                <Input label="Email" />
+                <Input label="Name" className="mb-2" {...bindInput('name')} />
+                <Input label="Email" {...bindInput('email')} />
             </div>
         </div>
     );
