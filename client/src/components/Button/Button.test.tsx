7ce4d28881e2e09df0d75a3210d23b2967883d0a
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-05-31 13:13:24 +0200

    Button tests

--- client/src/components/Button/Button.test.tsx
+++ client/src/components/Button/Button.test.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Button from '.';
 
+test('render label', () => {
+  const { getByText } = render(<Button label="click" onClick={() => null} />);
+  const element = getByText('click');
+  expect(element).toBeInTheDocument();
+});
+
 test('call onClick function on click', () => {
   const fn = jest.fn();
   const { getByText } = render(<Button label="click" onClick={fn} />);
   const element = getByText('click');
   fireEvent.click(element);
   expect(fn).toBeCalled();
+});
+
+test('render disabled class', () => {
+  const { getByText } = render(<Button label="click" onClick={() => null} disabled={true} />);
+  const element = getByText('click');
+  expect(element).toHaveClass('opacity-75 cursor-not-allowed');
+});
+
+test('do not call onClick function when disabled', () => {
+  const fn = jest.fn();
+  const { getByText } = render(<Button label="click" onClick={fn} disabled={true} />);
+  const element = getByText('click');
+  fireEvent.click(element);
+  expect(fn).toBeCalledTimes(0);
 });
