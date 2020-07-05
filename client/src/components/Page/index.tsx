c9d198da295303e56e7c8716e128953e2f7684a2
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-07-05 19:40:33 +0200

    Page title adjustment

--- client/src/components/Page/index.tsx
+++ client/src/components/Page/index.tsx
@@ -11,8 +11,8 @@ type PropTypes = {
 function Page({ children, title, newRoute, newLabel }: PropTypes) {
   return (
     <div className="mt-20 ml-64 w-full bg-gray-200 px-12 py-8">
-      <div className="flex justify-between">
-        <h3 className="text-gray-700 text-3xl font-semibold mb-4">{title}</h3>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-gray-700 text-4xl font-semibold">{title}</h1>
         {newLabel && <LinkButton icon="add" label={newLabel} to={newRoute} />}
       </div>
       {children}
