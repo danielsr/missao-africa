69c5c2bf8b7ea52db9177e09fc7689d80861aaa9
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-07-05 17:50:15 +0200

    Fix LabelGroup error

--- client/src/components/LabelGroup.tsx
+++ client/src/components/LabelGroup.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import LabelComponent from './Label';
 import { Label } from '../types';
 
-type props = {
+type PropTypes = {
   labels: Label[];
   value?: string[];
 };
 
-function LabelGroup({ labels, value }: props) {
+function LabelGroup({ labels, value }: PropTypes) {
   return (
     <div>
       {value?.map((labelName) => {
-        const label = labels.find((label) => label.name === labelName);
+        const label = labels?.find((label) => label.name === labelName);
         return <LabelComponent name={labelName} color={label?.color} key={labelName} />;
       })}
     </div>
