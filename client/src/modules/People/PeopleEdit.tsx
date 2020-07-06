c0718a20392b0b53992d498c277bb58167e2113e
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-07-06 16:21:40 +0200

    PeopleEdit feedback

--- client/src/modules/People/PeopleEdit.tsx
+++ client/src/modules/People/PeopleEdit.tsx
@@ -8,11 +8,13 @@ import { InputType } from 'components/Input';
 import LabelGroup from 'components/LabelGroup';
 import { useLabels } from 'modules/Labels/hooks';
 import { ButtonType, LinkButton } from 'components/Button';
+import Spinner from 'components/Spinner';
 
 function PeopleEdit() {
   const history = useHistory();
   const { id } = useParams();
   const [saving, setSaving] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { values, setValues, bindInput } = useForm({});
   const { showToaster } = useToaster();
   const { labels } = useLabels();
@@ -33,35 +35,46 @@ function PeopleEdit() {
   useEffect(() => {
     const fetchPerson = async () => {
       if (id > 0) {
-        const resp = await api.getPerson(id);
-        setValues(resp.data);
+        try {
+          setLoading(true);
+          const resp = await api.getPerson(id);
+          setValues(resp.data);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
       }
     };
     fetchPerson();
-  }, [id, setValues]);
+  }, [id, setValues, setLoading]);
 
   return (
     <Page title="People">
-      <Block>
-        <LabelGroup labels={labels} value={values.labels} />
-        <Input label="Name" className="mb-2 mt-4" {...bindInput('name')} />
-        <Input label="Email" className="mb-2" {...bindInput('email')} />
-        <Input label="CPF" className="mb-2" {...bindInput('cpf')} />
-        <Input label="Phone" className="mb-2" {...bindInput('phone')} />
-        <Input label="Address" className="mb-2" {...bindInput('address')} />
-        <Input label="Notes" className="mb-2" {...bindInput('notes')} />
-        <Input
-          label="Submited at"
-          type={InputType.datetimeLocal}
-          className="mb-2"
-          disabled
-          {...bindInput('submitedAt')}
-        />
-        <div className="flex mt-8">
-          <Button icon="save" label="Save" onClick={save} className="mr-2" working={saving} />
-          <LinkButton label="Cancel" type={ButtonType.Secondary} to="/people" />
-        </div>
-      </Block>
+      {!loading ? (
+        <Block>
+          <LabelGroup labels={labels} value={values.labels} />
+          <Input label="Name" className="mb-2 mt-4" {...bindInput('name')} />
+          <Input label="Email" className="mb-2" {...bindInput('email')} />
+          <Input label="CPF" className="mb-2" {...bindInput('cpf')} />
+          <Input label="Phone" className="mb-2" {...bindInput('phone')} />
+          <Input label="Address" className="mb-2" {...bindInput('address')} />
+          <Input label="Notes" className="mb-2" {...bindInput('notes')} />
+          <Input
+            label="Submited at"
+            type={InputType.datetimeLocal}
+            className="mb-2"
+            disabled
+            {...bindInput('submitedAt')}
+          />
+          <div className="flex mt-8">
+            <Button icon="save" label="Save" onClick={save} className="mr-2" working={saving} />
+            <LinkButton label="Cancel" type={ButtonType.Secondary} to="/people" />
+          </div>
+        </Block>
+      ) : (
+        <Spinner />
+      )}
     </Page>
   );
 }
