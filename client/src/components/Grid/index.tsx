b169881872697cad2c995816009bd384014d31f2
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-07-19 01:08:01 +0200

    Grid refactor

---
+++ client/src/components/Grid/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export type GridField = {
+  name: string;
+  label: string;
+  align?: string;
+  renderFunction?: Function;
+};
+
+export interface GridProps {
+  fields: GridField[];
+  data: any[];
+}
+
+function Header({ fields }: { fields: GridField[] }) {
+  return (
+    <thead>
+      <tr>
+        {fields.map((field) => (
+          <th
+            key={`Grid_Header_${field.name}`}
+            className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+          >
+            {field.label}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+}
+
+function Body({ fields, data }: { fields: GridField[]; data: any[] }) {
+  return (
+    <tbody className="bg-white">
+      {data.map((row, index) => (
+        <tr key={`Grid_Row_${index}`}>
+          {fields.map((field) => (
+            <td
+              key={`Grid_Row_${index}_${field.name}`}
+              className="px-6 py-4 truncate border-b border-gray-200"
+            >
+              {field.renderFunction ? field.renderFunction(row) : row[field.name]}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
+function Grid({ fields, data }: GridProps) {
+  return (
+    <div className="align-middle inline-block w-full shadow overflow-hidden rounded border-b border-gray-200 text-gray-900 text-sm">
+      <table className="w-full">
+        <Header fields={fields} />
+        <Body fields={fields} data={data} />
+      </table>
+    </div>
+  );
+}
+
+export default Grid;
