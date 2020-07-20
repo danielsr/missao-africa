28cd4a34baf73c6a3aaa946949dcc7b63ddb205c
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-07-20 13:17:06 +0200

    Sidemenu active class name

--- client/src/components/SideMenu/index.tsx
+++ client/src/components/SideMenu/index.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { Link, useLocation, matchPath } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Icon from 'components/Icon';
 
 function SideMenu() {
-  const { pathname } = useLocation();
-
   const menuItems = [
     { label: 'People', to: '/people', icon: 'face' },
     { label: 'Import People', to: '/people-import', icon: 'publish' },
     { label: 'Labels', to: '/labels', icon: 'label' },
-  ].map((menuItem) => ({ ...menuItem, active: matchPath(pathname, { path: menuItem.to }) }));
-
-  const baseClass = 'flex items-center mt-4 py-2 px-6 block border-l-4';
-  const regularClass = `${baseClass} border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100`;
-  const activeClass = `${baseClass} bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100`;
+  ];
 
   return (
-    <nav className="fixed top-0 left-0 bottom-0 w-64 bg-gray-900 flex flex-col text-white mt-20">
-      {menuItems.map(({ label, to, active, icon }) => (
-        <Link key={label} to={to} className={active ? activeClass : regularClass}>
+    <nav className="fixed top-0 left-0 bottom-0 w-64 bg-gray-900 flex flex-col text-white mt-20 pt-4">
+      {menuItems.map(({ label, to, icon }) => (
+        <NavLink
+          key={label}
+          to={to}
+          className="flex items-center mt-4 px-6 py-2 text-gray-300 hover:bg-gray-600 hover:bg-opacity-25"
+          activeClassName="border-r-4 border-gray-300 bg-gray-600 bg-opacity-25"
+        >
           <Icon icon={icon} />
           <span className="ml-3">{label}</span>
-        </Link>
+        </NavLink>
       ))}
     </nav>
   );
