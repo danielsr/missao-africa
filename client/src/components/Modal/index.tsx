ecde6365c6debaf67c5eb423244b3b09d3b07865
Daniel Rodrigues <daniel@ticketinggroup.com>
2020-07-25 11:48:40 +0200

    Context menu TopBar

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -24,27 +24,25 @@ function Modal({ size = ModalSize.Auto, title, children, footer, header, closeRo
   const bodyClass = `modalBody ${size} bg-white rounded shadow-lg flex flex-col`;
 
   return (
-    <>
-      <div className="modal fixed top-0 bottom-0 left-0 right-0 z-10 flex justify-center items-center">
-        <div className={bodyClass}>
-          <div className="bg-white rounded-t border-b-2 relative">
-            {title && <h1 className="text-gray-700 text-2xl font-semibold p-4">{title}</h1>}
-            {header?.()}
-            {closeRoute && (
-              <div className="absolute right-0 top-0 mt-4 mr-4">
-                <Link to={closeRoute}>
-                  <Icon icon="close" />
-                </Link>
-              </div>
-            )}
-          </div>
-          <div className="bg-gray-200 rounded p-6 overflow-scroll flex-1">{children}</div>
-          {footer && (
-            <div className="bg-white rounded-b border-t-2 flex p-4 justify-center">{footer()}</div>
+    <div className="modal fixed top-0 bottom-0 left-0 right-0 z-10 flex justify-center items-center">
+      <div className={bodyClass}>
+        <div className="bg-white rounded-t border-b-2 relative">
+          {title && <h1 className="text-gray-700 text-2xl font-semibold p-4">{title}</h1>}
+          {header?.()}
+          {closeRoute && (
+            <div className="absolute right-0 top-0 mt-4 mr-4">
+              <Link to={closeRoute}>
+                <Icon icon="close" />
+              </Link>
+            </div>
           )}
         </div>
+        <div className="bg-gray-200 rounded p-6 overflow-scroll flex-1">{children}</div>
+        {footer && (
+          <div className="bg-white rounded-b border-t-2 flex p-4 justify-center">{footer()}</div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
--- client/src/components/TopBar/index.tsx
+++ client/src/components/TopBar/index.tsx
@@ -5,19 +5,22 @@ import Icon from 'components/Icon';
 import { useUser } from 'store/user/hooks';
 import { logout } from 'services/auth';
 
-function ContextMenu() {
+function ContextMenu({ onClickOutside }: { onClickOutside: Function }) {
   return (
-    <div className="absolute shadow bg-white right-0 w-auto mr-4 mt-32 rounded flex flex-col">
-      <button className="p-3 border-b hover:bg-gray-200 text-left focus:outline-none">
-        Change password
-      </button>
-      <button
-        className="p-3 border-b hover:bg-gray-200 text-left focus:outline-none"
-        onClick={logout}
-      >
-        Logout
-      </button>
-    </div>
+    <>
+      <div className="absolute shadow bg-white right-0 w-auto mr-4 mt-32 rounded flex flex-col z-20">
+        <button className="p-3 border-b hover:bg-gray-200 text-left focus:outline-none">
+          Change password
+        </button>
+        <button
+          className="p-3 border-b hover:bg-gray-200 text-left focus:outline-none"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </div>
+      <div className="fixed top-0 bottom-0 left-0 right-0" onClick={() => onClickOutside()} />
+    </>
   );
 }
 
@@ -27,13 +30,13 @@ function TopBar() {
   const toggleMenu = () => setShowMenu((showMenu) => !showMenu);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 h-20 bg-white shadow flex justify-between p-3 items-center">
+    <nav className="fixed top-0 left-0 right-0 h-20 bg-white shadow flex justify-between p-3 items-center z-10">
       <img className={style.logo} src={logo} alt="Missao Africa" />
       <div className="flex mr-4 cursor-pointer" onClick={toggleMenu}>
         <span className="mr-2">{user?.name}</span>
         <Icon icon="account_circle" />
       </div>
-      {showMenu && <ContextMenu />}
+      {showMenu && <ContextMenu onClickOutside={toggleMenu} />}
     </nav>
   );
 }
